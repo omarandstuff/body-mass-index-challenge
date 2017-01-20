8fe4cf54e10b10eab10d8a03664d0aaefa17dd20
David De Anda <omarandstuff@gmail.com>
2017-01-20 15:55:43 -0600

    CORS allowed from the keroku app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins 'https://body-mass-index-app.herokuapp.com'
 
     resource '*', :headers => :any, :methods => [:get, :post, :patch, :delete, :options]
   end
