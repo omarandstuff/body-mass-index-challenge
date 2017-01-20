1161fd4a4ceded6a1f91db45d0df4a69a8f5928c
David De Anda <omarandstuff@gmail.com>
2017-01-19 21:14:54 -0600

    Change to postgresql db

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,15 @@
 
 ActiveRecord::Schema.define(version: 20170120023100) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "mass_index_records", force: :cascade do |t|
     t.decimal  "body_max_index", default: "0.0", null: false
     t.integer  "user_id"
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
-    t.index ["user_id"], name: "index_mass_index_records_on_user_id"
+    t.index ["user_id"], name: "index_mass_index_records_on_user_id", using: :btree
   end
 
   create_table "sessions", force: :cascade do |t|
@@ -26,17 +29,19 @@ ActiveRecord::Schema.define(version: 20170120023100) do
     t.integer  "user_id"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-    t.index ["user_id"], name: "index_sessions_on_user_id"
+    t.index ["user_id"], name: "index_sessions_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",      null: false
-    t.string   "password",   null: false
+    t.string   "email"
+    t.string   "password_digest"
     t.string   "firstname"
     t.string   "lastname"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
+  add_foreign_key "mass_index_records", "users"
+  add_foreign_key "sessions", "users"
 end
