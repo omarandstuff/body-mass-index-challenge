034423ccd02527e8a91d25da948d66b4db25d58a
David De Anda <omarandstuff@gmail.com>
2017-01-19 20:29:59 -0600

    Mass index records table

---
+++ db/migrate/20170120022556_create_mass_index_records.rb
@@ -0,0 +1,12 @@
+class CreateMassIndexRecords < ActiveRecord::Migration[5.0]
+  def change
+    create_table :mass_index_records do |t|
+      t.decimal :body_max_index, null: false, default: 0.0
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+
+    add_index :mass_index_records, :user_id
+  end
+end
