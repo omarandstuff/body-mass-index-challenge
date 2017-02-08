a5059552c9fefb63f345757959805725a0e4ab8f
David De Anda <omarandstuff@gmail.com>
2017-02-08 16:20:46 -0600

    Use retrieve service in controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
   def login
-    session = retrieve_session email: params[:email], password: params[:password], token: request.headers[:HTTP_CSRF_TOKEN]
+    session = Sessions::Retrive email: params[:email], password: params[:password], token: request.headers[:HTTP_CSRF_TOKEN]
 
     if session && session.errors.empty?
       render json: { user: session.user, token: session.token }
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
     user = User.new user_params
 
     if user.save
-      session = retrieve_session email: user.email, password: user.password, token: nil
+      session = Sessions::Retrive email: user.email, password: user.password, token: nil
 
       if session && session.errors.empty?
         render json: { user: user, token: user.sessions.first.token }
@@ -45,40 +45,5 @@ class SessionsController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:email, :firstname, :lastname, :password)
-    end
-
-    def retrieve_session(token:, email:, password:)
-      # Maybe the session already exists
-      if token
-        session = Session.find_by_token(token)
-
-        # The session exists, if it isn't active, it has to be owned by the user with the same email
-        # as the suplied to be reactivated
-        if session && (session.active || session.user.email == email)
-          authorized = session.active || session.user.authenticate(password)
-
-          if authorized && !session.active
-            session.active = true
-            session.save
-            return session
-          end
-
-          return authorized ? session : nil
-        end
-      end
-
-      # No valid token was suplied, lets try to authentificate a new session
-      user = User.find_by_email(email)
-
-      # The credentials are not valid
-      if !(user && user.authenticate(password))
-        return nil
-      end
-
-      new_token = Sessions::GenerateToken.new(user.id, Time.now).process
-
-      session = Session.new token: new_token, user: user
-      session.save
-      return session
     end
 end
