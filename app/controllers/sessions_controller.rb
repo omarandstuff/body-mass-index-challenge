dd8283a0f161af75e1b9e681dae432c57df3f809
David De Anda <omarandstuff@gmail.com>
2017-02-08 16:47:42 -0600

    One more typo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
   def login
-    session = Sessions::Retrieve.new(email: params[:email], password: params[:password], token: request.headers[:HTTP_CSRF_TOKEN]).preocess
+    session = Sessions::Retrieve.new(email: params[:email], password: params[:password], token: request.headers[:HTTP_CSRF_TOKEN]).process
 
     if session && session.errors.empty?
       render json: { user: session.user, token: session.token }
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
     user = User.new user_params
 
     if user.save
-      session = Sessions::Retrieve.new(email: user.email, password: user.password, token: nil).preocess
+      session = Sessions::Retrieve.new(email: user.email, password: user.password, token: nil).process
 
       if session && session.errors.empty?
         render json: { user: user, token: user.sessions.first.token }
