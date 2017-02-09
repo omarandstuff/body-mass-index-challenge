a3144e6161d45a47f6db7560bf593d9d7c887b50
David De Anda <omarandstuff@gmail.com>
2017-02-09 11:09:00 -0600

    Now tests are more self explanatory

--- spec/services/sessions/retrieve_spec.rb
+++ spec/services/sessions/retrieve_spec.rb
@@ -2,67 +2,72 @@ require 'rails_helper'
 
 describe Sessions::Retrieve do
   describe 'process' do
-    before(:each) do
-      @user1 = FactoryGirl.create(:david)
-      @user2 = FactoryGirl.create(:omar)
-
-      @active_user1_token = Sessions::GenerateToken.new(@user1.id, 2.seconds.ago).process
-      @active_user2_token = Sessions::GenerateToken.new(@user2.id, 2.seconds.ago).process
-
-      @active_user1_session = Session.create! user: @user1, token: @active_user1_token
-      @active_user2_session = Session.create! user: @user2, token: @active_user2_token
-
-      @inactive_user1_token = Sessions::GenerateToken.new(@user1.id, 2.hours.ago).process
-      @inactive_user2_token = Sessions::GenerateToken.new(@user2.id, 2.hours.ago).process
-
-      @inactive_user1_session = Session.create! user: @user1, token: @inactive_user1_token, active: false
-      @inactive_user2_session = Session.create! user: @user2, token: @inactive_user2_token, active: false
-    end
 
     context 'when the token is present' do
       it 'returns a session object' do
-        service = Sessions::Retrieve.new(token: @active_user1_token)
+        user = FactoryGirl.create(:david)
+        token = Sessions::GenerateToken.new(user.id, Time.now).process
+        session = Session.create! user: user, token: token
 
-        expect(service.process).to eq @active_user1_session
+        service = Sessions::Retrieve.new(token: token)
+
+        expect(service.process).to eq session
       end
 
       context 'and session is active' do
         it 'returns an active session object' do
-          service = Sessions::Retrieve.new(token: @active_user1_token)
+          user = FactoryGirl.create(:david)
+          token = Sessions::GenerateToken.new(user.id, Time.now).process
+          session = Session.create! user: user, token: token
 
-          expect(service.process).to eq @active_user1_session
+          service = Sessions::Retrieve.new(token: token)
 
-          @active_user1_session.reload
+          expect(service.process).to eq session
 
-          expect(@active_user1_session).to be_active
+          session.reload
+
+          expect(session).to be_active
         end
       end
 
       context 'and session is inactive' do
         it 'returns nil' do
-          service = Sessions::Retrieve.new(token: @inactive_user1_token)
+          user = FactoryGirl.create(:david)
+          token = Sessions::GenerateToken.new(user.id, Time.now).process
+          session = Session.create! user: user, token: token, active: false
+
+          service = Sessions::Retrieve.new(token: token)
 
           expect(service.process).to eq nil
         end
 
         it 'reactivates the session if the right credentials are given' do
-          service = Sessions::Retrieve.new(email: @user1.email, password: @user1.password, token: @inactive_user1_token)
+          user = FactoryGirl.create(:david)
+          token = Sessions::GenerateToken.new(user.id, Time.now).process
+          session = Session.create! user: user, token: token, active: false
 
-          expect(service.process).to eq @inactive_user1_session
+          service = Sessions::Retrieve.new(email: user.email, password: user.password, token: token)
 
-          @inactive_user1_session.reload
+          expect(service.process).to eq session
 
-          expect(@inactive_user1_session).to be_active
+          session.reload
+
+          expect(session).to be_active
         end
 
         it 'does not reactivate the session if the user is not the owner' do
-          service = Sessions::Retrieve.new(email: @user2.email, password: @user2.password, token: @inactive_user1_token)
+          user = FactoryGirl.create(:david)
+          fake_user = FactoryGirl.create(:omar)
+          token = Sessions::GenerateToken.new(user.id, Time.now).process
+          session = Session.create! user: user, token: token, active: false
+
+          service = Sessions::Retrieve.new(email: fake_user.email, password: fake_user.password, token: token)
 
           expect(service.process).to eq nil
 
-          @inactive_user1_session.reload
+          session.reload
 
-          expect(@inactive_user1_session).to_not be_active
+          expect(session).to_not be_active
         end
       end
     end
@@ -76,7 +81,11 @@ describe Sessions::Retrieve do
 
       context 'and the right credentials are given' do
         it 'returns a new active session' do
-          service = Sessions::Retrieve.new(email: @user1.email, password: @user1.password, token: 'erratic token')
+          user = FactoryGirl.create(:david)
+          token = Sessions::GenerateToken.new(user.id, Time.now).process
+
+          service = Sessions::Retrieve.new(email: user.email, password: user.password, token: 'erratic token')
+
           expect{ service.process }.to change(Session, :count).by(1)
           expect(Session.last).to be_active
         end
@@ -84,7 +93,7 @@ describe Sessions::Retrieve do
 
       context 'and the wrong credentials are given' do
         it 'returns nil' do
-          service = Sessions::Retrieve.new(email: 'wron_gemail', password: 'wrong_password', token: 'erratic token')
+          service = Sessions::Retrieve.new(email: 'wrong_email', password: 'wrong_password', token: 'erratic token')
 
           expect(service.process).to eq nil
         end
@@ -94,7 +103,11 @@ describe Sessions::Retrieve do
     context 'when there is no token' do
       context 'and the right credentials are given' do
         it 'returns a new active session' do
-          service = Sessions::Retrieve.new(email: @user1.email, password: @user1.password)
+          user = FactoryGirl.create(:david)
+          token = Sessions::GenerateToken.new(user.id, Time.now).process
+
+          service = Sessions::Retrieve.new(email: user.email, password: user.password)
+
           expect{ service.process }.to change(Session, :count).by(1)
           expect(Session.last).to be_active
         end
